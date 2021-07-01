12b9c4c65d7f55c9195424434149dd25c5df74fd
OnurSaatcioglu <onursaatcioglu@hotmail.com>
2021-07-01 21:29:16 +0300

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAssignment.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Department { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateUpdated { get; set; }
+
+    }
+}
