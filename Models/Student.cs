c2497ff24cfaef99af00f1aa31c6e434f7792f3e
OnurSaatcioglu <onursaatcioglu@hotmail.com>
2021-07-01 21:40:36 +0300

    Create ve Edit sayfalarından yaş, oluşturulma tarihi ve güncellenme tarihi formları kaldırıldı.
    Doğum tarihi formu sadece tarih olacak şekilde değiştirildi.

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,6 +16,7 @@ namespace StudentAssignment.Models
 
         public string Department { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
 
         public int Age { get; set; }
